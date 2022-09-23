0145c29c279b56d01c3b1521dba75e3ece52a3a8
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-09-23 10:49:15 +0200

    Add working solution to both ex2 & ex3

--- src/main.go
+++ src/main.go
@@ -2,57 +2,70 @@ package main
 
 import (
 	"fmt"
-	"math"
-
+	"math/big"
 )
 
 const (
-	SHARED_BASE float64 = 666;
-	SHARED_PRIME float64 = 6661;
+	SHARED_BASE  int64 = 666
+	SHARED_PRIME int64 = 6661
 )
 
 func main() {
 
-	var bobPk float64 = 2227;
+	bobPk := big.NewInt(2227)
+	// Assignment 1
+	alicePk, messageCipher := encrypt(*big.NewInt(6), *bobPk, *big.NewInt(2000))
+	fmt.Printf("Alice's public key is: %s, and the encrypted message is: %s\n", alicePk.Text(10), messageCipher.Text(10))
 
-	aliceKey, messageCipher := encrypt(8, bobPk, 2000);
-	fmt.Printf("Alice's key is: %.0f, and the encrypted message is: %.0f\n", aliceKey, messageCipher);
+	// Assignment 2
+	bobSecret, message := intercept(*bobPk, *alicePk, *messageCipher)
+	fmt.Printf("Bob's secret is: %s, and the decrypted message is: %s\n", bobSecret.Text(10), message.Text(10))
 
-	bobSecret, message := intercept(bobPk, messageCipher);
-	fmt.Printf("Bob's secret is: %.0f, and the decrypted message is: %.0f", bobSecret, message);
-
+	// Assignment 3
+	modifiedCipher := messageCipher.Mul(big.NewInt(3), messageCipher);
+	bobMessage := decrypt(*alicePk, bobSecret, *modifiedCipher)
+	fmt.Printf("Message after Mallory has tampered with the ciphertext: %s\n", bobMessage.Text(10))
 }
 
-func calculateKey(base, prime, secret float64) float64 {
+func calculateKey(base, prime, secret big.Int) *big.Int {
 
-	return math.Mod(math.Pow(base, secret), prime);
+	result := big.NewInt(0);
+	result.Exp(&base, &secret, nil);
+	result.Mod(result, &prime);
+	return result;
 }
 
-func encrypt(secretKey, publicKey, text float64) (key, cipher float64) {
-	fmt.Printf("Encrypting...\n");
+func encrypt(secretKey, publicKey, text big.Int) (key, cipher *big.Int) {
+	fmt.Printf("Encrypting...\n")
+	sharedBase := big.NewInt(SHARED_BASE)
+	sharedPrime := big.NewInt(SHARED_PRIME)
 
-	secret := calculateKey(SHARED_BASE, SHARED_PRIME, secretKey);
-	commonKey := calculateKey(publicKey, SHARED_PRIME, secretKey); 
-	cipherText := text * commonKey;
+	secret := calculateKey(*sharedBase, *sharedPrime, secretKey)
+	commonKey := calculateKey(publicKey, *sharedPrime, secretKey)
+	cipher = big.NewInt(0);
+	cipher.Mul(commonKey, &text)
 
-	return secret, cipherText;
+	return secret, cipher
 }
 
-func decrypt(commonKey, cipher float64) float64 {
-	message := cipher / commonKey;
-	return message;
+func decrypt(pk, secret, cipher big.Int) big.Int {
+	sharedKey := calculateKey(pk, *big.NewInt(SHARED_PRIME), secret);
+
+	result := big.NewInt(0);
+	return *result.Div(&cipher, sharedKey);
 }
 
-func intercept(publicKey, cipher float64) (secret, message float64) {
-
-	var testSecret float64;
-	for testSecret = 1; testSecret < 1000; testSecret++ {
-		
-		if key := calculateKey(SHARED_BASE, SHARED_PRIME, testSecret); key == publicKey {
-
-			message := decrypt(key, cipher);
-			return testSecret, message;
+func intercept(targetPk, pk, cipher big.Int) (secret, message big.Int) {
+	var limit big.Int = *big.NewInt(1000)
+	incrementer := big.NewInt(1)
+	var testSecret big.Int
+	for testSecret = *big.NewInt(1); testSecret.Cmp(&limit) < 0; testSecret.Add(&testSecret, incrementer) {
+		key := calculateKey(*big.NewInt(SHARED_BASE), *big.NewInt(SHARED_PRIME), testSecret)
+		if key.Cmp(&targetPk) == 0 {
+			
+			message := decrypt(pk, testSecret, cipher)
+			return testSecret, message
 		}
-	}	
-	return 0, 0;
+	}
+	return *big.NewInt(0), *big.NewInt(0)
 }
